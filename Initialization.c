09bd1f428815d93478519d89cc55209803e44c06
mbj0702 <mbj0702@gmail.com>
2022-01-05 06:56:19 -0500

    Initialization added

---
+++ Initialization.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  
+  int numOfBooks = 5;
+  char favLetter = 'A';
+  char favDigit = '2';
+  double costOfCandyBar = 1.99; 
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
