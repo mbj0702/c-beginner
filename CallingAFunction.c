1b766561f2a7b933b0e64ce09c5adc9c6db0b48b
mbj0702 <mbj0702@gmail.com>
2022-01-23 02:16:04 -0500

    CallingAFunction added

---
+++ CallingAFunction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  // Write your code below
+  printf("Welcome to Hell!");
+  int num1 = 1;
+  printf("Welcome to Hell!: %d\n", num1);
+  int num2 = 2;
+  printf("Welcome to Hell!: %d %d\n", num1, num2);
+
+}
