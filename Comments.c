9e7384d7b0c9f89a01276971100b7891a422bda9
mbj0702 <mbj0702@gmail.com>
2022-01-23 03:34:44 -0500

    Comments added

---
+++ Comments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // Chocolate chip cookies
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* C Programming language */
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
